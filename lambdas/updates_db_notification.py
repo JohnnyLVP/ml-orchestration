60f232087e97994f5df0a82a6682fab03b49e1cd
Johnny Leandro Valenzuela Phocco <jvalenzuela@belcorp.biz>
2020-02-05 19:01:04 -0500

    change in lambda

--- lambdas/updates_db_notification.py
+++ lambdas/updates_db_notification.py
@@ -1,11 +1,26 @@
 import json
+import os
+from botocore.vendored import requests
+from constants.regular_constants import *
+from utils.orchestrator_utils import OrchestratorManager
+from constants.process_stages_enum import PipelineStages
+from constants.db_status_enum import DBStatus
+from constants.common_constants import CommonConstants
+
 
 
 def lambda_handler(event, context):
-    print(json.dumps(event))
+    #print(json.dumps(event))
 
     try:
+        message = json.loads(event['Records'][0]['body'])
 
-        message = get_message_elements()
-
-    except Exception as e: 
+        if message.get('info'):
+            print("Write in Info DynamoTable...") #Lambda Trigger process siempre tiene que manda este update
+        
+        else:
+            print("Write in Processing DynamoTable...")
+            
+            
+    except Exception as e:
+        print('Exception has ocurred: {}'.format(str(e)))
