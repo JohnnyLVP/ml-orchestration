7e12d72281411876bf1a545868bb5abde0fc98e2
JohnnyLVP <johnnyvalenzuelap@gmail.com>
2020-02-16 18:43:07 -0500

    Adding comments

--- lambdas/post_processing_notif.py
+++ lambdas/post_processing_notif.py
@@ -1,1 +1,12 @@
 import json
+
+'''
+    - Cambair status de 0 a 1 en el json.
+    - Send notification to process if success
+    - Send notification to update success or failed. 
+    
+'''
+
+def lambda_handler(event, context):
+
+    print(json.dumps(event))
