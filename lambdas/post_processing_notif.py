b17452aff0dc3d30071d293d9d407ab546a177e9
Johnny Leandro Valenzuela Phocco <jvalenzuela@belcorp.biz>
2020-02-19 17:37:07 -0500

    Adding post lambda

--- lambdas/post_processing_notif.py
+++ lambdas/post_processing_notif.py
@@ -1,43 +1,33 @@
 import json
 from utils.orchestrator_utils import OrchestratorManager
 from utils.sns import SNSManager
-
-'''
-    - Cambair status de 0 a 1 en el json.
-    - Send notification to process if success
-    - Send notification to update success or failed. 
-    
-'''
+from constants.db_status_enum import DBStatus
+from constants.process_stages_enum import PipelineStages
 
 def lambda_handler(event, context):
-    
+
     print(json.dumps(event))
     sns_manager = SNSManager(os.environ['REGION'])
-    
-    try:
-        
-        if event['process_info']['stage'] == 'ALGORITHM' and event['process_info']['status'] == 'SUCCEEDED':
-            #SNS message with init_process +1 
-            #process message 
-            #updates message 
-        
-        elif event['process_info']['stage'] == 'ALGORITHM' and event['process_info']['status'] == 'FAILED':
-            #SNS message with init_process +1 with failed
-            #process message
-            #updates message 
 
-    except Exception, e:
-        raise e
+    try:
+        process_response = post_process_message(event, sns_manager)
+        print("Process message was sent: {}".format(process_response))
+
+        update_response = post_updates_message(event, sns_manager)
+        print("Update message was sent: {}".format(update_response))
+
+    except Exception as e:
+        print("Exception has ocurred while sending the sns message:{}".format(str(e)))
 
 
 def post_process_message(event, sns_manager):
-    
+
     mlo_manager = OrchestratorManager()
-    
+
     s3_bucket = os.enviorn['S3_BUCKET']
     file_path = os.environ['PATH_NAME']
 
-    algorithm_list = mlo_manager.get_json_list(s3_bucket,file_path)
+    algorithm_list = mlo_manager.get_json_list(s3_bucket, file_path)
     process_message = {}
     #{"country": "CR", "campaign": "202001", "env": "DEV", "message_type": "process", "process_type": "SecuentialProcess", "init_process": 0}
     try:
@@ -46,22 +36,38 @@ def post_process_message(event, sns_manager):
         process_message['env'] = event['env']
         process_message['message_type'] = event['message_type']
         process_message['process_type'] = event['process_type']
-        
-        current_execution_order = event['init_process']
-        if current_execution_order < len(algorithm_list['Process'][event['process_type']])
-            process_message['init_process'] = current_execution_order + 1
-        
-        response = sns_manager.publish_message(process_message, os.environ['SNS_TOPIC_ARN'])
 
-    except Exception, e: 
+        current_execution_order = event['init_process']
+        if current_execution_order < len(algorithm_list['Process'][event['process_type']]):
+            process_message['init_process'] = current_execution_order + 1
+
+        response = sns_manager.publish_message(
+            process_message, os.environ['SNS_TOPIC_ARN'])
+
+    except Exception, e:
         print("Exception has ocurred: {}".format(e))
-    
-def post_updates_message(event):
+
+    return response
+
+
+def post_updates_message(event, sns_manager):
     #"{\"uuid\": \"59ec7fb6-54ba-4335-b747-f4e070bd2c5b\", \"timestamp\": 1580933339888, \"stage\": \"REQUEST\", \"status\": \"FAILED\", \"failure_reason\": \"None\", \"message_type\": \"updates\"}",
     update_message = {}
     try:
         update_message['uuid'] = event['uuid']
         update_message['timestamp'] = event['process_info']['created_at']
-        update_message['']
+        update_message['stage'] = PipelineStages.post_process
+        update_message['status'] = event['process_info']['status']
+        update_message['failure_reason'] = event['process_info']['failure_reason']
+        update_message['message_type'] = 'updates'
 
-    
+        if event['process_info']['status'] == DBStatus.failed and event['process_info']['stage'] = PipelineStages.algorithm:
+            update_message['algorithm'] == event['info']['algoritmo']
+
+        response = sns_manager.publish_message(
+            update_message, os.environ['SNS_TOPIC_ARN'])
+
+    except Exception as e:
+        print("Exception has ocurred:{}".format(str(e)))
+
+    return response
