e2586168b4a0631b1e84e38151158fa128ed149e
Johnny Leandro Valenzuela Phocco <jvalenzuela@belcorp.biz>
2020-03-09 10:30:20 -0500

    Adding process info

--- lambdas/submit_request.py
+++ lambdas/submit_request.py
@@ -1,11 +1,12 @@
 import json
 import os
 import requests
-from constants.regular_constants import mlo_uuid, request_ok_status,request_created_status, none_failure_reason, request_status, process_status, uuid, process_info, status, country, campaign, info, algortihm_payload, endpoint
+from constants.regular_constants import mlo_uuid, request_ok_status,request_created_status, none_failure_reason, request_status, process_status, uuid, process_info, status, country, campaign, info, algortihm_payload, endpoint, stage, created_at, failure_reason
 from utils.orchestrator_utils import OrchestratorManager
 from constants.process_stages_enum import PipelineStages
 from constants.db_status_enum import DBStatus
 from constants.common_constants import CommonConstants
+from constants.process_stages_enum import PipelineStages
 
 
 def lambda_handler(event, context):
@@ -22,21 +23,22 @@ def lambda_handler(event, context):
     event[process_info] = {}
     
     try:
+        event[process_info][stage] = PipelineStages.request
+        event[process_info][created_at] = OrchestratorManager.get_current_timestamp()
+
         # process campaign update message
         response = get_response_train_request(event)
 
         print(response)
-        print(response.content)
-        
+        print(response.content)   
 
         if response.status_code in [request_ok_status, request_created_status]:
-            failure_reason = none_failure_reason
             event[request_status] = CommonConstants.request_succeded
-            event[process_info][status] = DBStatus.submitted
+            event[process_info][status] = DBStatus.submitted 
         else:
-            failure_reason = response.content
             event[request_status] = CommonConstants.request_failed
             event[process_info][status] = DBStatus.failed
+            event[process_info][failure_reason] = none_failure_reason
 
         mlo_manager.get_process_status_updates(
             region_name=aws_region,
@@ -44,19 +46,19 @@ def lambda_handler(event, context):
             uniq_id=event[uuid],
             proc_stage=PipelineStages.request,
             proc_status=DBStatus.submitted,
-            failure_reason=failure_reason
+            failure_reason=event[process_info][failure_reason]
         )
 
     except Exception as e:
         print("Exception in process_campaign_update_notif lambda: {}".format(str(e)))
-        failure_reason = str(e)[:150]
+        event[process_info][failure_reason] = str(e)[:150]
         mlo_manager.get_process_status_updates(
             region_name=aws_region,
             update_topic_arn=update_topic,
             uniq_id=event[uuid],
             proc_stage=PipelineStages.request,
             proc_status=DBStatus.failed,
-            failure_reason=failure_reason
+            failure_reason=event[process_info][failure_reason]
         )
         event[request_status] = CommonConstants.request_failed
         event[process_info][status] = DBStatus.failed
